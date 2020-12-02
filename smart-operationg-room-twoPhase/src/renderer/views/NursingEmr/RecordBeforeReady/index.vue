f028597553df0df90652d522c3b0c1c76e3de892
dyw-design <1875742887@qq.com>
2020-12-02 09:45:50 +0800

    fx

---
+++ smart-operationg-room-twoPhase/src/renderer/views/NursingEmr/RecordBeforeReady/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="record-before">
+    123
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RecordBefore',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .record-before{
+
+  }
+</style>
---
+++ smart-operationg-room-twoPhase/src/renderer/views/NursingEmr/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="emr">
+    <view-router />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
