18f1ce046dd6c6d2177ec4ba7ab230dada3f70bd
dyw-design <1875742887@qq.com>
2021-01-08 18:36:36 +0800

    fx

--- NrWorks-PDA/vue_pages/src/config/url-config.js
+++ NrWorks-PDA/vue_pages/src/config/url-config.js
@@ -3,8 +3,8 @@ if (process.env.NODE_ENV === 'production') {
   /// 生产环境api接口地址
   api = {
     // baseURL: 'http://192.168.1.106:5090'
-    // baseURL: 'http://128.0.18.38:5090'
     baseURL: 'http://128.0.18.38:5090'
+    // baseURL: 'http://128.0.18.38:5090'
     // baseURL: 'http://192.168.1.175:8090'
     // baseURL: 'http://47.103.105.200:8090'
   }
